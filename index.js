953de4bcc2223aafda8dedde33b5cc11d5b368ec
Anshuman Gupta <anshuman844@gmail.com>
2020-03-06 14:50:56 +0530

    Added middleware to allow CORS request.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/api/whoami', (req, res) => {
   res.json({
     ipaddress: req.ip,
