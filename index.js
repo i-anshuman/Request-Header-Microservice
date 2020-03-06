013aa77730e3eae834887112341f41e4a4a8dabd
Anshuman Gupta <anshuman844@gmail.com>
2020-03-06 14:48:19 +0530

    Request Header Microservice Created.   - Send a json date with three fields.   - IP address of client.   - Language from request header.   - System info from request header.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("request header microerice is coming soon.") ;
+const express = require('express');
+const app = express();
+
+app.get('/api/whoami', (req, res) => {
+  res.json({
+    ipaddress: req.ip,
+    language: req.get('Accept-Language'),
+    software: req.get('User-Agent')
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
